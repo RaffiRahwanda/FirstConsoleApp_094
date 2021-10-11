9fef596e72694c179cc649171406d6856d2713a2
Raffi Rahwanda <raffirhwnda27@gmail.com>
2021-10-11 18:10:44 +0700

    Add project files.

---
+++ FirstConsoleApp_094/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace FirstConsoleApp_094
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!\t");
+            Console.WriteLine("My name is Raffi Rahwanda");
+        }
+    }
+}
